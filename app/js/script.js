a980760e85e311672fae177a883683abe1244846
Simon Gagnon <simongag93@gmail.com>
2021-04-30 14:41:02 -0400

    added click on burger to show/hide overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,15 @@
 const hamburger = document.querySelector('.header_hamburger');
+const burgerMenu = document.querySelector('.header_overlay');
+
+
 
 hamburger.addEventListener('click', function() {
-    console.log("clicked burger")
+
+    if(burgerMenu.classList.contains('hidden')) {
+        burgerMenu.classList.add('show');
+        burgerMenu.classList.remove('hidden');
+    } else {
+        burgerMenu.classList.add('hidden');
+        burgerMenu.classList.remove('show');
+    }
 });
