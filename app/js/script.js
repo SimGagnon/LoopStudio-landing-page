b63ec7b573ae0270808688e7dca4a0f8bdf29cda
Simon Gagnon <gagnonbsimon@gmail.com>
2021-05-07 13:59:27 -0400

    finished burger animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.header_hamburger');
 const burgerMenu = document.querySelector('.header_overlay');
 const body = document.querySelector('body');
+const mobileList = document.querySelectorAll(".header_overlay li");
 
 
 
@@ -9,9 +10,20 @@ hamburger.addEventListener('click', function () {
     if (!hamburger.classList.contains('open')) {
         burgerMenu.classList.add('show');
         hamburger.classList.add('open');
+        body.classList.add('noscroll');
+        mobileList.forEach(function (e) {
+            e.classList.add("slide_in");
+            e.classList.remove("fade_out");
+        })
+        // body.classList.add('noscroll');
         
     } else {
         hamburger.classList.remove('open');
         burgerMenu.classList.remove('show');
+        body.classList.remove('noscroll');
+        mobileList.forEach(function (e) {
+            e.classList.remove("slide_in");
+            e.classList.add("fade_out");
+        })
     }
 });
